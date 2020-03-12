c950932d938c3e8f914f9df52f59d099637751e2
AliSlouma <alislouma9@gmail.com>
2020-03-12 22:54:33 +0200

    heap array

--- src/Heap.java
+++ src/Heap.java
@@ -1,25 +1,41 @@
 import java.util.ArrayList;
 import java.util.Collection;
 
-public class Heap <T extends Comparable<T>> implements IHeap<T> {
+public class Heap <T extends Comparable<T>> implements IHeap<T> ,Cloneable{
 
-    ArrayList<IHeap> nodes ;
+    ArrayList<INode> nodes;
 
-    public Heap(ArrayList<IHeap> nodes) {
-        this.nodes = nodes;
+    public Heap() {
+        this.nodes = new ArrayList<>();
     }
 
     public INode<T> getRoot() {
-
-
-        return (INode<T>) this.nodes.get(0);
+        return this.nodes.get(0);
     }
 
     public int size() {
         return this.nodes.size();
     }
 
+    public void swap(INode<T> node1,INode<T> node2){
+        T temp = node1.getValue();
+        node1.setValue(node2.getValue());
+        node2.setValue(node1.getValue());
+    }
+
     public void heapify(INode<T> node) {
+
+        if(node.getValue().compareTo(node.getLeftChild().getValue()) < 0 || node.getValue().compareTo(node.getRightChild().getValue() ) <0 ){
+            if(node.getLeftChild().getValue().compareTo(node.getRightChild().getValue())<0){
+                swap(node,node.getRightChild());
+                heapify(node.getRightChild());
+
+            }else {
+                swap(node,node.getLeftChild());
+                heapify(node.getLeftChild());
+            }
+
+        }
 
     }
 
@@ -28,10 +44,23 @@ public class Heap <T extends Comparable<T>> implements IHeap<T> {
     }
 
     public void insert(T element) {
+        INode tmep = new Node(nodes.size(),nodes);
+        nodes.add(tmep);
+        int pos =nodes.size();
+        int parent = pos/2;
+        while (nodes.get(pos).getValue().compareTo(nodes.get(parent).getValue()) >0){
+            swap(nodes.get(pos),nodes.get(parent));
+            pos =parent;
+            parent = pos/2;
+        }
 
     }
 
     public void build(Collection<T> unordered) {
+
+
+
+
 
     }
 }
--- src/Node.java
+++ src/Node.java
@@ -1,27 +1,30 @@
+import java.util.ArrayList;
+
 public class Node<T extends Comparable<T>> implements INode {
 
-    Node leftChild;
-    Node rightChild;
-    Node parent;
+    INode leftChild =null;
+    INode rightChild = null;
+    INode parent = null;
     T value;
+    int indx;
+    ArrayList<IHeap> nodes;
 
-    public Node(Node leftChild, Node rightChild) {
-        this.leftChild = leftChild;
-        this.rightChild = rightChild;
+    public <T extends Comparable<T>> Node(int indx , ArrayList<IHeap> nodes) {
+        this.indx = indx;
+        this.nodes = nodes;
     }
-
     public INode <T>getLeftChild() {
-        return this.leftChild;
+        return (INode<T>) nodes.get(indx*2+1);
     }
 
     @Override
     public INode <T> getRightChild() {
-        return this.rightChild;
+        return (INode<T>) nodes.get(indx*2+2);
     }
 
     @Override
     public INode <T>getParent() {
-        return this.parent;
+        return(INode<T>) nodes.get(indx/2);
     }
 
     @Override
